aed51fb82f195b5bd1fb7a7d168152f06951c6d9
michael-maton <michael@nonfungible.com>
2023-11-30 18:36:36 -0600

    flap functionality

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 },
+      gravity: { y: 400 },
+      debug: true,
     },
   },
   scene: {
     preload,
     create,
+    update,
   },
 };
 
@@ -24,8 +26,23 @@ function preload() {
 }
 
 let bird = null;
+const flapVELOCITY = 250;
 
 function create() {
   this.add.image(config.width / 2, config.height / 2, 'sky');
-  this.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0);
+  bird = this.physics.add
+    .sprite(config.width / 10, config.height / 2, 'bird')
+    .setOrigin(0);
+
+  var spaceBar = this.input.keyboard.addKey(
+    Phaser.Input.Keyboard.KeyCodes.SPACE
+  );
+  this.input.on('pointerdown', flap);
+  spaceBar.on('down', flap);
 }
+
+const flap = () => {
+  bird.body.velocity.y = -flapVELOCITY;
+};
+
+function update(time, delta) {}
