0c50a9ccab95f24999a67e2ca63526548e00d778
michael-maton <michael@nonfungible.com>
2023-12-07 01:52:18 -0600

    playscene refactor

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import Phaser from 'phaser';
+import PlayScene from './scenes/PlayScene.js';
+
+const WIDTH = 1000;
+const HEIGHT = 600;
+const POSITION = { x: WIDTH / 10, y: HEIGHT / 2 };
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  initial_position: POSITION,
+};
 
 const config = {
   type: Phaser.AUTO,
-  width: 1000,
-  height: 600,
+  ...SHARED_CONFIG,
   physics: {
     default: 'arcade',
     arcade: {
       // debug: true,
     },
   },
-  scene: {
-    preload,
-    create,
-    update,
-  },
+  scene: [new PlayScene(SHARED_CONFIG)],
 };
 
 new Phaser.Game(config);
---
+++ src/scenes/PlayScene.js
@@ -0,0 +1,25 @@
+import Phaser from 'phaser';
+
+class PlayScene extends Phaser.Scene {
+  constructor(config) {
+    super('PlayScene');
+    this.config = config;
+
+    this.bird = null;
+  }
+
+  preload() {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('bird', 'assets/bird.png');
+  }
+
+  create() {
+    this.add.image(0, 0, 'sky').setOrigin(0);
+    this.bird = this.physics.add.sprite(this.config.initial_position.x, this.config.initial_position.y, 'bird').setOrigin(0);
+    this.bird.body.gravity.y = 400;
+  }
+
+  update() {}
+}
+
+export default PlayScene;
